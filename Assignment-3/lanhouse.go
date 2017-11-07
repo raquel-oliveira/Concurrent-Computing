b72811eb6a00f2ed9f535a8d94f0b39319c34ab8
Raquel LOPES DE OLIVEIRA <oliveira.raquel.lopes@gmail.com>
2017-11-07 15:25:48 -0300

    Update solution (lanhouse)

--- Assignment-3/lanhouse.go
+++ Assignment-3/lanhouse.go
@@ -1,23 +1,23 @@
 package main
 
 import(
-"fmt"
-"math/rand"
-"time"
+	"fmt"
+	"math/rand"
+	"time"
+	"strconv"
+	"sync"
 )
-const minTime = 15;
-const maxTime = 120;
 
-/*
- Generates a random number given a range
- @param min minimum number
- @param max maximum number
- @return a random number between min and max
+const (
+	MIN_TIME = 15;
+	MAX_TIME = 120;
+	NUMBER_COMPUTERS = 8;
+	NUMBER_TEENAGERS = 26;
+)
 
- source: source: http://golangcookbook.blogspot.com.br/2012/11/generate-random-number-in-given-range.html
-*/
+// source: source: http://golangcookbook.blogspot.com.br/2012/11/generate-random-number-in-given-range.html
 func random(min, max int) int {
-	rand.Seed(time.Now().Unix())
+	//rand.Seed(time.Now().Unix())
 	return rand.Intn(max - min) + min
 }
 
@@ -29,29 +29,72 @@ func teenName(i int) string {
 }
 
 
-func useComputer(in <- chan string){
+func waitingRoom(in chan string, out chan string){
+	waitingPeople := []string{}
+
+
 	for {
-		user, check := <-in;
-		if !check{
-			return ;
+		select{
+			case user :=<- in:
+				if len(waitingPeople) > 0 {
+					fmt.Println(user + "está aguardando");
+					waitingPeople = append(waitingPeople, user);
+				} else{
+					select {
+						case out <- user:
+							break;
+						default:
+							fmt.Println(user + "está aguardando");
+							waitingPeople = append(waitingPeople, user);
+					}
+				}
+			default:
+				if (len(waitingPeople) > 0){
+					out <-waitingPeople[0]
+					waitingPeople = waitingPeople[1:]
+				}  else{
+					//break;
+					close(out)
+					return;
+				}
 		}
-		fmt.Println(string(user) + "está on-line ");
-		interval := random(minTime, maxTime);
-		time.Sleep(time.Duration(interval/interval) * time.Second);
-		fmt.Println(string(user) + "liberou a máquina após passar ", interval, " minutos on-line");
 	}
 }
 
-func main(){
-	numberComputers := 8;
-	numberTeenagers := 26;
-	//a := make(chan string);
-	b := make(chan string);
-	for computer := 0; computer < numberComputers; computer++ {
-		go useComputer(b);
+func useComputer(in chan string, wg *sync.WaitGroup){
+//	defer wg.Done();
+	for {
+		user, check := <-in;
+		if !check{
+			break ;
+		}
+		fmt.Println(user + "está on-line ");
+		interval := random(MIN_TIME, MAX_TIME);
+		time.Sleep(time.Duration(interval/5) * time.Second);
+		fmt.Println(user + "liberou a máquina após passar "+ strconv.Itoa(interval) +" minutos on-line");
 	}
-	for teen := 0; teen < numberTeenagers ; teen++  {
-		b <- (teenName(teen));
+	wg.Done()
+}
+
+func main(){
+	users := make(chan string, NUMBER_TEENAGERS);
+	var wg sync.WaitGroup;
+	c := make(chan string);
+
+	for computer := 0; computer < NUMBER_COMPUTERS; computer++ {
+		wg.Add(1);
+		go useComputer(c, &wg);
+	}
+
+	go waitingRoom(users, c);
+
+	for teen := 0; teen < NUMBER_TEENAGERS ; teen++  {
+		users <- (teenName(teen));
 
 	}
+
+	//close(users);
+	wg.Wait();
+	fmt.Println("A lan-house está finalmente vazia e todos foram atendidos")
+
 }
